2d4c7b6684095d54451fd813cf063f4a3613527e
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2012-02-24 11:16:35 +0100

    Added dash user styles

--- style/style-2.py
+++ style/style-2.py
@@ -10,16 +10,29 @@ Y = np.ones(X.size)
 
 # Actual plotting
 fig = plt.figure(figsize=(8,6), dpi=72, facecolor="white")
-axes = plt.subplot(111)
-axes.plot( X, Y*0.2, color = 'blue', linewidth=2, linestyle="-" )
-axes.plot( X, Y*0.4, color = 'blue', linewidth=2, linestyle="--" )
-axes.plot( X, Y*0.6, color = 'blue', linewidth=2, linestyle="-." )
-axes.plot( X, Y*0.8, color = 'blue', linewidth=2, linestyle=":" )
+axes = plt.subplot(111,aspect=1)
+axes.plot( X, Y*0.1, color = 'blue', linewidth=2, linestyle="-" )
+axes.plot( X, Y*0.2, color = 'blue', linewidth=2, linestyle="--" )
+axes.plot( X, Y*0.3, color = 'blue', linewidth=2, linestyle="-." )
+axes.plot( X, Y*0.4, color = 'blue', linewidth=2, linestyle=":" )
+line, = axes.plot( X, Y*0.5, color = 'blue', linewidth=2, linestyle="-" )
+line.set_dashes([20,2])
+line, = axes.plot( X, Y*0.6, color = 'blue', linewidth=2, linestyle="-" )
+line.set_dashes([2,20])
+line, = axes.plot( X, Y*0.7, color = 'blue', linewidth=2, linestyle="-" )
+line.set_dashes((40,5,5,5))
+line, = axes.plot( X, Y*0.8, color = 'blue', linewidth=2, linestyle="-" )
+line.set_dashes((40,5,5,5,5,5))
+line, = axes.plot( X, Y*0.9, color = 'blue', linewidth=2, linestyle="-" )
+line.set_dashes((40,5,5,5,5,5,40,5))
+
 
 axes.set_xlim(X.min(),X.max())
 axes.set_ylim(0,1)
 axes.set_xticks([])
-axes.set_yticks((0.2,0.4,0.6,0.8))
-axes.set_yticklabels(("-","--","-.",":","."))
+axes.set_yticks(np.arange(1,10)/10.0)
+axes.set_yticklabels(("-","--","-.",":",
+                      "(20,2)", "(2,20)", "(40,5,5,5)",
+                      "(40,5,5,5,5,5,5)", "(40,5,5,5,5,40)"))
 
 plt.show()
